d5bcf14c8500413bfa56315104e1cf3e2de9fdfb
Israfil007 <israfil964946@gmail.com>
2025-08-08 16:03:41 +0600

    adding some new line

--- first.py
+++ first.py
@@ -1,3 +1,9 @@
-print("This line is the first line I wrote in python")
-print("This line is the first line I wrote in python")
+print ("This line is the first line I wrote in python")
 
+print("Hello world")
+
+print ("*" * 10)
+
+print ("hello world")
+
+2+3
