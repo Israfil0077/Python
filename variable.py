994fc5eefd5ef3e9ccffa67d221d235242950a5c
Israfil007 <israfil964946@gmail.com>
2025-08-08 16:53:05 +0600

    Adding a new file with a new set of code about variables

---
+++ variable.py
@@ -0,0 +1,24 @@
+students_count = 1000  # Integer variable
+rating = 4.99  # Float variable
+is_published = False  # Boolean varibale
+course_name = "Python programming"  # string variable
+print(students_count)
+
+print(len(course_name))  # to find the lenght of a string
+print(course_name[0])  # First letter of the string
+print(course_name[-1])  # output- "g"
+print(course_name[0:3])  # slice string: First 3 characters
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])  # copying the string..
+
+
+# Escape sequence...
+course = "Pyhton \n Programming"  # New line in python
+print(course)
+
+# Formatted String...concateninng string
+first = "Israfil"
+last = "Hossain"
+full = first + " "+last
+print(full)
