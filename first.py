f991297d4f24d0a3c5d48030a04bb5aed097d19f
Israfil007 <israfil964946@gmail.com>
2025-08-08 23:32:10 +0600

    comment

--- first.py
+++ first.py
@@ -10,3 +10,9 @@ x = 1
 
 y = 2
 unit_price = 3
+
+# This is a Single line comment...
+
+"""This is a Multiple Line comment.
+    these are mostly used for documentation of function, classes and modules...
+"""
