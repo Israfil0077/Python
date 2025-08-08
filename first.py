3bc7506271c9df7a1fd6837d5c7d29cee47d3e71
Israfil007 <israfil964946@gmail.com>
2025-08-08 16:20:16 +0600

    Commiting

--- first.py
+++ first.py
@@ -1,9 +1,12 @@
-print ("This line is the first line I wrote in python")
+print("This line is the first line I wrote in python")
 
 print("Hello world")
 
-print ("*" * 10)
+print("*" * 10)
 
-print ("hello world")
+print("hello world")
 
-2+3
+x = 1
+
+y = 2
+unit_price = 3
