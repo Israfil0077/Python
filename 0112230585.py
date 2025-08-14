947e7bcf384c85470c704a9b09d1bc6b7120e0ae
Israfil007 <israfil964946@gmail.com>
2025-08-14 16:21:42 +0600

    hfkds

--- 0112230585.py
+++ 0112230585.py
@@ -1,11 +1,11 @@
-arr1 = [[[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]]]
-arr2 = [[[[[1, 9], [3, 0]], [[5, 6], [0, 8]]]]]
+Arrary1 = [[[[[8, 9], [3, 4]], [[9, 10], [2, 14]]]]]
+Arrary2 = [[[[[18, 9], [3, 12]], [[9, 8], [9, 14]]]]]
 common_values = []
-for i in range(len(arr1)):
-    for j in range(len(arr1[i])):
-        for k in range(len(arr1[i][j])):
-            for l in range(len(arr1[i][j][k])):
-                for m in range(len(arr1[i][j][k][l])):
-                    if arr1[i][j][k][l][m] == arr2[i][j][k][l][m]:
-                        common_values.append(arr1[i][j][k][l][m])
+for i in range(len(Arrary1)):
+    for j in range(len(Arrary1[i])):
+        for k in range(len(Arrary1[i][j])):
+            for l in range(len(Arrary1[i][j][k])):
+                for m in range(len(Arrary1[i][j][k][l])):
+                    if Arrary1[i][j][k][l][m] == Arrary2[i][j][k][l][m]:
+                        common_values.append(Arrary1[i][j][k][l][m])
 print("Common values:", common_values)
