0aceb0a78f57884a1f12f5737211f17d2a7c415a
Israfil007 <israfil964946@gmail.com>
2025-08-14 22:17:20 +0600

    hdjdk

--- 0112230585.py
+++ 0112230585.py
@@ -1,13 +1,26 @@
-# Comparison between two 5D arrays' elements  for common data
+# Declare 5D arrays
+Array1 = [[[[[2, 3], [18, 12], [9, 10], [2, 14]]]]]
+Array2 = [[[[[18, 9], [3, 12], [6, 8], [9, 14]]]]]
 
-Arrary1 = [[[[[8, 9], [3, 4]], [[9, 10], [2, 14]]]]]
-Arrary2 = [[[[[18, 9], [3, 12]], [[9, 8], [9, 14]]]]]
+
+# Store common elements
 common_values = []
-for i in range(len(Arrary1)):
-    for j in range(len(Arrary1[i])):
-        for k in range(len(Arrary1[i][j])):
-            for l in range(len(Arrary1[i][j][k])):
-                for m in range(len(Arrary1[i][j][k][l])):
-                    if Arrary1[i][j][k][l][m] == Arrary2[i][j][k][l][m]:
-                        common_values.append(Arrary1[i][j][k][l][m])
+
+# Compare each element in both arrays
+for i in range(len(Array1)):
+    for j in range(len(Array1[i])):
+        for k in range(len(Array1[i][j])):
+            for l in range(len(Array1[i][j][k])):
+                for m in range(len(Array1[i][j][k][l])):
+                    val1 = Array1[i][j][k][l][m]
+                    for i2 in range(len(Array2)):
+                        for j2 in range(len(Array2[i2])):
+                            for k2 in range(len(Array2[i2][j2])):
+                                for l2 in range(len(Array2[i2][j2][k2])):
+                                    for m2 in range(len(Array2[i2][j2][k2][l2])):
+                                        val2 = Array2[i2][j2][k2][l2][m2]
+                                        if val1 == val2 and val1 not in common_values:
+                                            common_values.append(val1)
+
+# Print common elements
 print("Common values:", common_values)
